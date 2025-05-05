b4d88b17b789af88435e765f2e0d0658dafb6fe9
cozmyc <172064888+CozmycDev@users.noreply.github.com>
2025-05-05 08:36:03 -0400

    More intuitive permission handling

--- src/main/java/net/doodcraft/cozmyc/fireproof/Fireproof.java
+++ src/main/java/net/doodcraft/cozmyc/fireproof/Fireproof.java
@@ -1,17 +1,50 @@
 package net.doodcraft.cozmyc.fireproof;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.FireAbility;
 import com.projectkorra.projectkorra.ability.PassiveAbility;
-import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.configuration.Config;
 import com.projectkorra.projectkorra.configuration.ConfigManager;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginManager;
 
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 
 public class Fireproof extends FireAbility implements AddonAbility, PassiveAbility {
+
+    public static final String PERMISSION_BASE = "bending.ability.fireproof";
+    public static final String MAGMA_PERMISSION = PERMISSION_BASE + ".magma";
+    public static final String LAVA_PERMISSION = PERMISSION_BASE + ".lava";
+
+    public static final Map<EntityDamageEvent.DamageCause, Predicate<Player>> CANCEL_PREDICATES =
+            Map.ofEntries(
+                    Map.entry(EntityDamageEvent.DamageCause.FIRE,
+                            player -> player.hasPermission(PERMISSION_BASE)),
+                    Map.entry(EntityDamageEvent.DamageCause.FIRE_TICK,
+                            player -> player.hasPermission(PERMISSION_BASE)),
+                    Map.entry(EntityDamageEvent.DamageCause.HOT_FLOOR,
+                            player -> ConfigManager.defaultConfig.get()
+                                    .getBoolean("ExtraAbilities.Cozmyc.Fireproof.MagmaResistance")
+                                    && hasEffectivePermission(player, MAGMA_PERMISSION)),
+                    Map.entry(EntityDamageEvent.DamageCause.LAVA,
+                            player -> ConfigManager.defaultConfig.get()
+                                    .getBoolean("ExtraAbilities.Cozmyc.Fireproof.LavaResistance")
+                                    && hasEffectivePermission(player, LAVA_PERMISSION))
+            );
+
+    private static boolean hasEffectivePermission(Player player, String specific) {
+        if (player.isPermissionSet(specific)) {
+            return player.hasPermission(specific);
+        }
+        return player.hasPermission(PERMISSION_BASE);
+    }
 
     public Fireproof(Player player) {
         super(player);
@@ -31,12 +64,12 @@ public class Fireproof extends FireAbility implements AddonAbility, PassiveAbili
     }
 
     @Override
-    public boolean isIgniteAbility() {
+    public boolean isInstantiable() {
         return false;
     }
 
     @Override
-    public boolean isExplosiveAbility() {
+    public boolean isProgressable() {
         return false;
     }
 
@@ -46,8 +79,28 @@ public class Fireproof extends FireAbility implements AddonAbility, PassiveAbili
     }
 
     @Override
+    public Location getLocation() {
+        return this.player.getLocation();
+    }
+
+    @Override
     public String getName() {
         return "Fireproof";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "LuxaelNI, Cozmyc";
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.0.2";
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
@@ -63,54 +116,38 @@ public class Fireproof extends FireAbility implements AddonAbility, PassiveAbili
     }
 
     @Override
-    public Location getLocation() {
-        return this.player.getLocation();
-    }
-
-    @Override
     public void load() {
-        ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new FireproofListener(), ProjectKorra.plugin);
+        ProjectKorra.plugin.getServer()
+                .getPluginManager()
+                .registerEvents(new FireproofListener(), ProjectKorra.plugin);
 
-        ConfigManager.defaultConfig.get().addDefault("ExtraAbilities.Cozmyc.Fireproof.LavaResistance", false);
-        ConfigManager.defaultConfig.get().addDefault("ExtraAbilities.Cozmyc.Fireproof.MagmaResistance", false);
+        PluginManager pm = ProjectKorra.plugin.getServer().getPluginManager();
+        for (String node : new String[]{MAGMA_PERMISSION, LAVA_PERMISSION}) {
+            if (pm.getPermission(node) == null) {
+                Permission perm = new Permission(node,
+                        "Bypass " + node.substring(node.lastIndexOf('.')+1) + " damage");
+                perm.addParent(PERMISSION_BASE, true);
+                pm.addPermission(perm);
+            }
+        }
 
-        ConfigManager.defaultConfig.save();
+        Config config = ConfigManager.defaultConfig;
 
-        ConfigManager.languageConfig.get().addDefault("Abilities.Fire.Fireproof.Description",
+        config.get().addDefault("ExtraAbilities.Cozmyc.Fireproof.MagmaResistance", false);
+        config.get().addDefault("ExtraAbilities.Cozmyc.Fireproof.LavaResistance", false);
+        config.save();
+
+        Config lang = ConfigManager.languageConfig;
+
+        lang.get().addDefault("Abilities.Fire.Fireproof.Description",
                 "Skilled Firebenders are able to control the heat of surrounding fire, allowing them to avoid burn damage.");
-        ConfigManager.languageConfig.get().addDefault("Abilities.Fire.Fireproof.Instructions",
+        lang.get().addDefault("Abilities.Fire.Fireproof.Instructions",
                 "Toggle this ability using /pk toggle FirePassives");
+        lang.save();
 
-        ConfigManager.languageConfig.save();
-
-        ProjectKorra.log.log(Level.INFO, "Fireproof 0.0.1 by LuxaelNI and Cozmyc is now enabled!");
+        ProjectKorra.log.log(Level.INFO, "Fireproof 0.0.2 by LuxaelNI and Cozmyc is now enabled!");
     }
 
     @Override
     public void stop() {}
-
-    @Override
-    public String getAuthor() {
-        return "LuxaelNI, Cozmyc";
-    }
-
-    @Override
-    public String getVersion() {
-        return "0.0.1";
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public boolean isInstantiable() {
-        return false;
-    }
-
-    @Override
-    public boolean isProgressable() {
-        return false;
-    }
 }
--- src/main/java/net/doodcraft/cozmyc/fireproof/FireproofListener.java
+++ src/main/java/net/doodcraft/cozmyc/fireproof/FireproofListener.java
@@ -2,47 +2,36 @@ package net.doodcraft.cozmyc.fireproof;
 
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.Element;
-import com.projectkorra.projectkorra.configuration.ConfigManager;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.util.function.Predicate;
+
 public class FireproofListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEntityDamageEvent(EntityDamageEvent event) {
-        Entity entity = event.getEntity();
-
-        if (!(entity instanceof Player player)) return;
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
 
         BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-        if (bPlayer == null || !bPlayer.canBendInWorld()) return;
-        if (!bPlayer.hasElement(Element.FIRE)) return;
-        if (!bPlayer.isPassiveToggled(Element.FIRE)) return;
-        if (!bPlayer.isToggledPassives()) return;
-        if (!player.hasPermission("bending.ability.fireproof")) return;
+        if (bPlayer == null
+                || !bPlayer.canBendInWorld()
+                || !bPlayer.hasElement(Element.FIRE)
+                || !bPlayer.isPassiveToggled(Element.FIRE)
+                || !bPlayer.isToggledPassives()) {
+            return;
+        }
 
-        if (event.getCause() == EntityDamageEvent.DamageCause.FIRE || event.getCause() == EntityDamageEvent.DamageCause.FIRE_TICK) {
+        Predicate<Player> canceller =
+                Fireproof.CANCEL_PREDICATES.get(event.getCause());
+
+        if (canceller != null && canceller.test(player)) {
             player.setFireTicks(0);
             event.setCancelled(true);
-        }
-
-        if (event.getCause() == EntityDamageEvent.DamageCause.HOT_FLOOR) {
-            if (ConfigManager.defaultConfig.get().getBoolean("ExtraAbilities.Cozmyc.Fireproof.MagmaResistance")) {
-                player.setFireTicks(0);
-                event.setCancelled(true);
-            }
-        }
-
-        if (event.getCause() == EntityDamageEvent.DamageCause.LAVA) {
-            if (ConfigManager.defaultConfig.get().getBoolean("ExtraAbilities.Cozmyc.Fireproof.LavaResistance")) {
-                player.setFireTicks(0);
-                event.setCancelled(true);
-            }
         }
     }
 }
